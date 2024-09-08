d98c88075e3eb92a5b1fe4a90e000f7b7356a7dc
SohailSajid <sohailsajid79@gmail.com>
2024-09-08 11:16:55 +0100

    re-upload solution2 with .sh script

---
+++ bash/projects/Bash Battle Arena GAME/02-Solution.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for i in {1..10}; do echo 'Number:' $i; done
+
+# Terminal Output
+
+#  /bin/bash "/Users/sohailsajid/Desktop/corderco-devops/bash/projects/Bash Battle Arena GAME/02-Solution.sh"
+# Number: 1
+# Number: 2
+# Number: 3
+# Number: 4
+# Number: 5
+# Number: 6
+# Number: 7
+# Number: 8
+# Number: 9
+# Number: 10
