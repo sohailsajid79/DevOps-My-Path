3c7db4b021583c3ee42c2f3be63a14cbc81779c3
SohailSajid <sohailsajid79@gmail.com>
2024-09-19 22:34:21 +0100

    upload ec2 module

---
+++ Terraform/AWS Project/terraform-aws-nginx-setup/modules/ec2/main.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "sg" {
+  name = "nginx-sg"
+
+  ingress {
+    description = "allow http traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nginx-sg"
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_pair
+
+  security_groups = [aws_security_group.sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y nginx
+              sudo systemctl start nginx
+              EOF
+
+  tags = {
+    Name = "ec2-nginx"
+  }
+}
---
+++ Terraform/AWS Project/terraform-aws-nginx-setup/modules/ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "key_pair" {
+  default = ""
+}
