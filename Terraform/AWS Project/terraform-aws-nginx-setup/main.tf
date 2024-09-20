5b4796abe60ffd392e3b8f375132c2992752679f
SohailSajid <sohailsajid79@gmail.com>
2024-09-20 23:42:55 +0100

    add key pair

--- Terraform/AWS Project/terraform-aws-nginx-setup/main.tf
+++ Terraform/AWS Project/terraform-aws-nginx-setup/main.tf
@@ -2,5 +2,5 @@ module "ec2" {
   source        = "./modules/ec2"
   instance_type = var.instance_type
   ami_id        = var.ami_id
-  key_pair      = var.key_pair
-}
+  key_name      = aws_key_pair.generate_key.key_name
+ }
